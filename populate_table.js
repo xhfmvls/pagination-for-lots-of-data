b106ce7fa3fbc1fec7b41576a9b5142c6b5764f4
vigil-senpai <vincent.pradipta@binus.ac.id>
2022-10-11 21:35:53 +0700

    feat: populate table script

--- populate_table.js
+++ populate_table.js
@@ -1,7 +1,6 @@
 import { faker } from '@faker-js/faker';
-import mysql from 'mysql2'; 
+import mysqlPromise from 'mysql2/promise';
 import dotenv from 'dotenv';
-import { exit } from 'process';
 dotenv.config(); 
 
 const connCreds = {
@@ -14,7 +13,7 @@ const connCreds = {
 const populateTable = async() => {
   const totalData = process.argv[2] || 5;
   const tableName = process.argv[3] || 'MsProduct'; 
-  const conn = mysql.createConnection(connCreds);
+  const conn = await mysqlPromise.createConnection(connCreds);
   console.log('[+] Database Connected');
 
   for(let i = 0; i < totalData; i++) {
@@ -24,14 +23,14 @@ const populateTable = async() => {
     const brand_name = faker.company.name(); 
     const retail_price = faker.commerce.price();
     const active_status = faker.datatype.boolean();
-    const date = faker.datatype.datetime({min: 1577836800000, max: 1893456000000}); 
 
-    const query = `INSERT INTO ${tableName} VALUES(${product_id}, ${product_name}, ${category}, ${brand_name}, ${retail_price}, ${active_status}, ${date})`;
-    
-    conn.promise()
+    const query = `INSERT INTO ${tableName}(product_id, product_name, category, brand_name, retail_price, active_status) VALUES('${product_id}', '${product_name}', '${category}', '${brand_name}', ${retail_price}, ${active_status})`;
+    await conn.execute(query);
   }
+
+  await conn.end(); 
   console.log(`[+] Successfully insert ${totalData} data`);
 }
 
 populateTable(); 
-exit();
+// exit();
