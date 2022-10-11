6b6adeecb14408b2cb8a22312c8b2c1f755e15e9
vigil-senpai <vincent.pradipta@binus.ac.id>
2022-10-11 20:27:02 +0700

    feat: create database script

---
+++ create-table.js
@@ -0,0 +1,35 @@
+import mysql from 'mysql2'; 
+import dotenv from 'dotenv';
+dotenv.config(); 
+
+const connCreds = {
+  host: process.env.DBHOST,
+  user: process.env.DBUSER, 
+  password: process.env.DBPASSWORD, 
+  database: process.env.DBNAME
+};
+
+const createTable = async () => {
+  const tableName = process.argv[2] || 'MsProduct'; 
+  const query = 
+  `CREATE TABLE ${tableName} (
+  product_id CHAR(36) NOT NULL, 
+  product_name VARCHAR(255) NOT NULL, 
+  category VARCHAR(64) NOT NULL, 
+  brand_name VARCHAR(255) NOT NULL, 
+  retail_price DECIMAL(15,2) NOT NULL, 
+  active_status BOOL NOT NULL, 
+  updated_at TIMESTAMP NOT NULL, 
+  created_at TIMESTAMP NOT NULL,
+  PRIMARY KEY(product_id))`;
+
+  const conn = mysql.createConnection(connCreds);
+  console.log('[+] Database Connected');
+  conn.promise()
+  .query(query)
+  .then(console.log("[+] Table Created"))
+  .catch(console.log)
+  .then((con) => conn.end());
+}
+
+createTable(); 
