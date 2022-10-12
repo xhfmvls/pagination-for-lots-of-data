ed71d27860f7c7ae3c47805917d304a730d13de4
vigil-senpai <vincent.pradipta@binus.ac.id>
2022-10-13 00:30:04 +0700

    fix: update product models and implement the offset

--- server/pkg/models/product.go
+++ server/pkg/models/product.go
@@ -1,22 +1,17 @@
 package models
 
 import (
-	"time"
-
 	"github.com/jinzhu/gorm"
 	"github.com/xhfmvls/pagination/pkg/config"
 )
 
 type Product struct {
-	ProductId    string    `json:product_id`
-	ProductName  string    `json:product_name`
-	Category      string    `json:category`
-	BrandName    string    `json:brand_name`
-	RetailPrice  float64   `json:retail_price`
-	ActiveStatus bool      `json:active_status`
-	CreatedAt    time.Time `json:created_at`
-	UpdatedAt    time.Time `json:updated_at`
-	DeletedAt    time.Time `json:deleted_at`
+	ProductId    string    `json:"product_id"`
+	ProductName  string    `json:"product_name"`
+	Category     string    `json:"category"`
+	BrandName    string    `json:"brand_name"`
+	RetailPrice  float64   `json:"retail_price"`
+	ActiveStatus bool      `json:"active_status"`
 }
 
 var db *gorm.DB
@@ -27,8 +22,8 @@ func init() {
 	db.AutoMigrate(&Product{})
 }
 
-func GetProducts() []Product {
-	var productsList *[]Product
-	db.Limit(10).Find(&productsList)
-	return *productsList
+func GetProducts(limit int, offset int) []Product {
+	var productsList []Product
+	db.Limit(limit).Offset(offset).Find(&productsList)
+	return productsList
 }
