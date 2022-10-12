dc73c6a7109c7f057c06183f5adee59ff3f55648
vigil-senpai <vincent.pradipta@binus.ac.id>
2022-10-12 23:47:45 +0700

    refactor: refactoring server code from start

--- server/main.go
+++ server/main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/joho/godotenv"
+)
+
+var NewRouter = func(router *mux.Router) {
+	// router.HandleFunc("/menu", controllers.PostFood).Methods("GET")
+}
+
+
 func main() {
-	LoadConfig()
+	r := mux.NewRouter().PathPrefix("/api").Subrouter()
+	godotenv.Load(".env")
+	port, portEnvErr := strconv.Atoi(os.Getenv("PORT"))
+	
+	if portEnvErr != nil {
+		panic("PORT not available")
+	}
+	
+	NewRouter(r)
+	http.Handle("/", r)
+	fmt.Printf("Server Running on Port %d\n", port)
+	address := fmt.Sprintf("localhost:%d", port)
+	log.Fatal(http.ListenAndServe(address, r))
 }
